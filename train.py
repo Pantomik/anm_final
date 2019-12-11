e39875e33ea65e5ccb4c30c7e63b3e7d612d9dd5
Marlon <marlon.l-huillier@epitech.eu>
2019-12-11 15:48:53 +0800

    Comment example code

--- train.py
+++ train.py
@@ -6,8 +6,8 @@ from plot import Plot
 def main():
     p = Parser()
     data = p.build_train_set('datasets/train.csv')
-    plot = Plot(data, 'train')
-    plot.display_plot(0)
+    # plot = Plot(data, 'test')
+    # plot.display_plot(0)
 
 
 if __name__ == '__main__':
